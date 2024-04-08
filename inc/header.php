27d2d2194b047d6efc65c45be10df82bf866e00c
Bruno Reche <49537489+briuno@users.noreply.github.com>
2024-04-08 17:21:53 -0300

    TESTE integração Hostinger

--- inc/header.php
+++ inc/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MoneyFive</title>
+    <title>MoneyFive TESTE</title>
     <link rel="stylesheet" href="css/styles.css"> <!-- Ajuste o caminho conforme necessário -->
 </head>
 <body>
