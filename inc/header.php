b1d206bd85d27109bdc146a3cbe5090260f4aae0
Bruno Reche <49537489+briuno@users.noreply.github.com>
2024-04-08 17:36:40 -0300

    Tente Hostinger 3

--- inc/header.php
+++ inc/header.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MoneyFive TESTE V1.2.0</title>
+    <title>MoneyFive</title>
     <link rel="stylesheet" href="css/styles.css"> <!-- Ajuste o caminho conforme necessário -->
 </head>
 <body>
     <header>
-        <h1><a href="index.php">MoneyFive</a></h1>
+        <h1><a href="index.php">MoneyFive TESTE V1.2.0</a></h1>
         <nav>
             <ul>
                 <li><a href="index.php">Home</a></li>
