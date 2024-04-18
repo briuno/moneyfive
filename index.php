3cf5ba52ee4b5e26226747afb88487fa761f7fad
Bruno Reche <49537489+briuno@users.noreply.github.com>
2024-04-18 16:23:40 -0300

    auth_check.php
    Atualização 1.1

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include('inc/header.php'); include('auth_check.php');?>
+<?php include('inc/header.php');?>
 <!-- Conteúdo da página inicial aqui -->
 <h1>Bem-vindo ao MoneyFive</h1>
 <p>Conectamos donos de pequenos e médios negócios com consultores financeiros para ajudar a tomar decisões informadas.</p>
