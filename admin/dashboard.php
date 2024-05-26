cf9620f409452754797a348fe95f782eb61385db
Bruno Reche <49537489+briuno@users.noreply.github.com>
2024-05-26 17:22:19 -0300

    Atualização: dashbord.php
    Melhorias aplicadas:
    
    1 Verificação de autenticação e autorização para garantir que apenas usuários administradores tenham acesso ao painel.
    
    2 Redirecionamento para a página de login caso o usuário não esteja autenticado ou não seja administrador.
    
    3 Correção do título da página para refletir o propósito do painel administrativo.

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,29 +2,29 @@
 include('auth_check.php');
 include('../config.php');
 session_start();
-?>
 
 // Verifique se o usuário está logado e se é um administrador
-// Esta verificação dependerá da sua implementação específica de controle de acesso
+if (!isset($_SESSION['user_id']) || $_SESSION['is_admin'] != 1) {
+    header('Location: ../login.php');
+    exit();
+}
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="../css/admin.css" media="screen" />
-  <title>Admin</title>
+  <title>Painel Administrativo</title>
 </head>
-
 <body>
-  <?php include('../inc/adminHeader.php');?>
+  <?php include('../inc/adminHeader.php'); ?>
   <article class="admin__container">
     <div class="wrapper__title">
       <h2 class="title">Painel Administrativo</h2>
       <p class="subtitle">Bem-vindo ao painel administrativo.</p>
     </div>
-
     <div class="admin__wrapper">
       <h1 class="">Operações</h1>
       <div class="buttons__wrapper">
@@ -36,5 +36,4 @@ session_start();
   </article>
   <?php include('../inc/adminFooter.php'); ?>
 </body>
-
 </html>
