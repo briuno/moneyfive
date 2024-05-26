783bda56a6d4bc4e322aa25515a7f5c03b6b3b32
Bruno Reche <49537489+briuno@users.noreply.github.com>
2024-05-26 17:40:05 -0300

    Atualização do HTML do Cabeçalho e Rodapé
    - Atualização do HTML do cabeçalho com links condicionais baseados na sessão do usuário.
    - Inclusão de links de navegação adicionais para usuários logados.
    - Melhoria da acessibilidade com uso de `aria-label`.
    - Adição de atributos `alt` descritivos para imagens.
    - Estrutura de navegação organizada e estilização consistente.
    - Manutenção do CSS inalterado conforme solicitado.

--- inc/header.php
+++ inc/header.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>MoneyFive</title>
   <link rel="stylesheet" type="text/css" href="css/header.css" media="screen" />
 </head>
-
 <body>
   <?php session_start(); ?>
   <header class="container">
@@ -37,5 +35,4 @@
     </nav>
   </header>
 </body>
-
 </html>
